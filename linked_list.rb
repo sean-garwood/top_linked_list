d099578f258a9abcac8cd425893bb81af33a4e28
sean-garwood <seansgarwood@gmaill.com>
2023-11-19 14:30:09 -0500

    Outline thoughts in comments

--- linked_list.rb
+++ linked_list.rb
@@ -4,6 +4,8 @@
 class Node
   attr_accessor :next, :data
 
+  # Each node consists of exactly two elements: data, and 'next', which is a
+  # link to the next node in the list.
   def initialize(data)
     @data = data
     @next = nil
@@ -20,6 +22,16 @@ end
 
 # Ruby impl of a linked list.
 class LinkedList
+
+  # A linked list consists of three sections: the head, an arbitrary number of
+  # nodes from zero to memory bound, and tail.
+
+  # Every list has to have a head and a tail, but not every list needs nodes.
+
+  # the head is the first element in the list
+
+  # the tail is the last element in the list
+
   def initialize
     @head = nil
     @tail = nil
