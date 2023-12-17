311cf5a66ae1ba51110b53b47e9abac03e5c8c0a
sean-garwood <seansgarwood@gmaill.com>
2023-12-17 11:02:59 -0500

    Initial commit
    Attempting to set @next_node dynamically on added nodes, or neighbor(s)
    of removed node.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,25 +4,48 @@ require_relative 'node'
 
 # Ruby impl of a linked list.
 class LinkedList
-attr_accessor :head, :tail
+attr_accessor :head, :tail, :nodes
 
 TRANSFORMATIONS = [[:pop, :remove_at], [:append, :prepend, :insert_at]]
-ADD_TO_LIST = TRANSFORMATIONS[1]
-REMOVE_FROM_LIST = TRANSFORMATIONS[0]
+ADD_NODE = TRANSFORMATIONS[1]
+REMOVE_NODE = TRANSFORMATIONS[0]
 
   def initialize
     @head = nil
     @tail = nil
+    @nodes = 0
   end
 
-  def make_node(value, type)
-    Node.new(value, transformation.call(type))
+  def size
+    nodes
   end
 
-  def transform(value, &type)
-    case
-    when type == :append
+  def transformation(type, &subtype)
+    type.zero? ? @nodes -= 1 : @nodes += 1
+    yield(subtype)
+  end
+
+  def make_node(value, transformation)
+    @nodes += 1
+    t = add_to_list_operation.call(transformation)
+    Node.new(value, t)
   end
 
 
-transformation = proc { |type, subtype| TRANSFORMATIONS[type][subtype] }
+  def pop
+    # some code
+  end
+
+  def append(value)
+    @head.nil?
+    # add node to end of list
+  end
+
+  def prepend(value)
+    # add a node to the beginning of list
+  end
+
+
+
+add_to_list_operation = proc { |subtype| ADD_NODE[subtype] }
+remove_from_list_operation = proc { |subtype| REMOVE_NODE[subtype] }
