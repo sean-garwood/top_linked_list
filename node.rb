1da7e96921ff1d54fb9539d12741e1dfbae26972
sean-garwood <seansgarwood@gmaill.com>
2023-12-16 23:06:38 -0500

    Modularize, organize code

---
+++ node.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# nodes in the linked list object
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
