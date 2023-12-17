ceecfb81148b1aad15b76c9851cb8bc110fed55c
sean-garwood <seansgarwood@gmaill.com>
2023-12-17 09:58:00 -0500

    Remove junk modules

--- modules/erase_node.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Removing a node from a linked list.
-module EraseNode
-  def erase_node(index, type)
-    type == 3 && @tail.next_node = nil
-    # when type == 4
-  end
-end
--- modules/node_handler.rb
+++
@@ -1,2 +0,0 @@
-require_relative './erase_node'
-require_relative './make_node'
